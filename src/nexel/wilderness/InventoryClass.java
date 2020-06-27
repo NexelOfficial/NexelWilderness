87e0d4ca98b80221b4925e2e1cb4e717f8dd1cb3
nathandiepeveen <nathandiepeveen.nl@gmail.com>
2020-06-27 09:09:38 +0200

    Allows main menu icons te be modified.

--- src/nexel/wilderness/InventoryClass.java
+++ src/nexel/wilderness/InventoryClass.java
@@ -27,6 +27,12 @@ public class InventoryClass {
 		mapChooserInventory = Bukkit.createInventory(null, 36, main.getConfig().getString("menuprefix"));
 		inventoryClearer(currentPlayer, 36);
 		
+		String backClose;
+		if (main.getConfig().isSet("mainIcons.backClose")) 
+			backClose = main.getConfig().getString("mainIcons.backClose");
+		else
+			backClose = "ARROW";
+		
 		if (main.getConfig().isSet("Biomes")) {
 			int i = 0;
 			for (String biome : main.getConfig().getConfigurationSection("Biomes").getKeys(false)) {
@@ -39,7 +45,7 @@ public class InventoryClass {
 			mapChooserInventory.setItem(0, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&cNo biomes have been set."), ChatColor.translateAlternateColorCodes('&', "&7&oUse the diamond to wild to a random biome.")));
 		}
 		
-		currentItem = new ItemStack(Material.ARROW);
+		currentItem = new ItemStack(Material.valueOf(backClose));
 		mapChooserInventory.setItem(27, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&cBack"), ChatColor.translateAlternateColorCodes('&', "&7&oClick to go back.")));
 		currentPlayer.openInventory(mapChooserInventory);
 		
@@ -47,17 +53,39 @@ public class InventoryClass {
 	
 	public void mainWildMenu(Player currentPlayer) {
 		
+		String helpCommand;
+		if (main.getConfig().isSet("mainIcons.helpCommand")) 
+			helpCommand = main.getConfig().getString("mainIcons.helpCommand");
+		else
+			helpCommand = "BOOK";
+		String randomBiome;
+		if (main.getConfig().isSet("mainIcons.randomBiome")) 
+			randomBiome = main.getConfig().getString("mainIcons.randomBiome");
+		else
+			randomBiome = "DIAMOND";
+		String backClose;
+		if (main.getConfig().isSet("mainIcons.backClose")) 
+			backClose = main.getConfig().getString("mainIcons.backClose");
+		else
+			backClose = "ARROW";
+		String biomePicker;
+		if (main.getConfig().isSet("mainIcons.biomePicker")) 
+			biomePicker = main.getConfig().getString("mainIcons.biomePicker");
+		else
+			biomePicker = "GRASS";
+			
+		
 		mapChooserInventory = Bukkit.createInventory(null, 27, main.getConfig().getString("menuprefix"));
 		inventoryClearer(currentPlayer, 27);
 		
-		currentItem = new ItemStack(Material.DIAMOND);
+		currentItem = new ItemStack(Material.valueOf(randomBiome));
 		mapChooserInventory.setItem(14, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&6Random biome"), ChatColor.translateAlternateColorCodes('&', "&7&oClick to teleport.")));
-		currentItem = new ItemStack(Material.GRASS);
+		currentItem = new ItemStack(Material.valueOf(biomePicker));
 		mapChooserInventory.setItem(12, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&aPick a biome"), ChatColor.translateAlternateColorCodes('&', "&7&oClick to open biome picker.")));
-		currentItem = new ItemStack(Material.ARROW);
+		currentItem = new ItemStack(Material.valueOf(backClose));
 		mapChooserInventory.setItem(18, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&cClose"), ChatColor.translateAlternateColorCodes('&', "&7&oClick to close.")));
 		if (currentPlayer.hasPermission("nexelwilderness.admin.help")) {
-			currentItem = new ItemStack(Material.BOOK);
+			currentItem = new ItemStack(Material.valueOf(helpCommand));
 			mapChooserInventory.setItem(26, newItem(currentItem, ChatColor.translateAlternateColorCodes('&', "&fUse /wild help for more options"), ChatColor.translateAlternateColorCodes('&', "&7&oClick to close and run /wild help.")));
 		}
 		currentPlayer.openInventory(mapChooserInventory);
